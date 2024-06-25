d711f0de3d8a0fe6235f987c33ade1dfd749f492
OhHyun <gamerleehun@gmail.com>
2024-06-25 10:41:51 +0900

    id 태그 추가 및 내용 수정

--- app/page.tsx
+++ app/page.tsx
@@ -27,14 +27,14 @@ export default function Home() {
         </nav>
 
       </header>
-      <section className="flex items-center justify-center w-full py-12 md:py-24 lg:py-32 xl:py-48">
+      <section className="flex items-center justify-center w-full py-12 md:py-24 lg:py-32 xl:py-48" id="home">
         <div className="container px-4 md:px-6">
           <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">권오현</h1>
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">게임 시나리오 포트폴리오</h1>
                 <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
-                  게임 시나리오 기획자가 되기위해 공부하고 있는 권오현 입니다.
+                  지금까지 작업한 작업물입니다.
                 </p>
               </div>
               <div className="flex flex-col gap-2 min-[400px]:flex-row">
